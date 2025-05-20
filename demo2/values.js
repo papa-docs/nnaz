994993b01ca7f1a1a20e716fcc917d5a4c122375
papa-docs <papa-docs1@proton.me>
2025-05-20 17:43:16 +0300

    Update values.js

--- demo2/values.js
+++ demo2/values.js
@@ -1,12 +1,12 @@
 // values.js
 
 // данные 
-var fio               = "Иванов Иван Иванович";
-var fio_en            = "Ivanov Ivan Ivanovich";
-var birth             = "01.01.2000";
-var rnokpp            = "1234567890";
+var fio               = "Игнатенко Назар Иванович";
+var fio_en            = "Ignatenko Nazar Ivanovich";
+var birth             = "17.04.1984";
+var rnokpp            = "1671653686";
 
-var prava_number      = "AB123456"; // номер прав
+var prava_number      = "GE653214"; // номер прав
 var university        = "ХНУ имени Каразина"; // Університет
 var fakultet          = "Физико-технический"; // Факультет
 var stepen_dip        = "Магістра";
@@ -16,7 +16,7 @@ var special_dip       = "Прикладная математика";
 var number_dip        = "MT-545678";
 var live              = "м. Харьков";
 var prava_date_out    = "01.01.2030";
-var bank_adress       = "м. Харьков, вул. Героїв, 123";
+var bank_adress       = "м. Дніпро, вул. Героїв, 123";
 var sex               = "М";
 var sex_en            = "M";
 
@@ -26,29 +26,29 @@ var student_date_give = "01.09.2021";
 var student_date_out  = "30.06.2025";
 var form              = "Очная";
 var rights_categories = "A, B";
-var date_give         = "01.01.2020";
-var prava_date_give   = "01.01.2020";
-var date_out          = "01.01.2030";
+var date_give         = "14.11.2022";
+var prava_date_give   = "14.11.2022";
+var date_out          = "14.11.2032";
 var pass_number       = "008759371";
 var organ             = "Шевченковский РОВД ХО";
 var uznr              = "106947";
-var legalAdress       = "м. Киев, вул. Свободи, 14";
+var legalAdress       = "м. Дніпро, вул. Свободи, 14";
 var registeredOn      = "13.05.2024";
 var pravaOrgan        = "ТСЦ 6345 МВС України";
 
 
 // Включение/выключение документов
 var isRightsEnabled   = true;
-var isStudyEnabled    = true;
+var isStudyEnabled    = false;
 var isZagranEnabled   = true;
 var isDiplomaEnabled  = true;
 // Добавь остальные переменные по аналогии
 
 // Пути к нужным фото    ?timestamp=N   "photo.jpg?timestamp=N" динамически: var url = "photo.jpg?timestamp=" + N;
 
-var photo_passport = "photo_passport.jpg"; //эДок, паспорт,
-var photo_rights   = "photo_rights.jpg"; //права
-var photo_students = "photo_students.jpg"; //диплом, студ
-var photo_zagran   = "photo_zagran.jpg"; //загран
+var photo_passport = "photo.jpg"; //эДок, паспорт,
+var photo_rights   = "photoR.jpg"; //права
+var photo_students = "photo.jpg"; //диплом, студ
+var photo_zagran   = "photo.jpg"; //загран
 // Можно добавить еще, если надо
 
